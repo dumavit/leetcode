25b81e6ed26a80cac934169d18daeffb7eb846ff
dumavit <vitalii.duma@corva.ai>
2020-05-30 14:34:28 +0300

    more tasks

--- K Closest Points to Origin.py
+++ K Closest Points to Origin.py
@@ -3,11 +3,8 @@ from typing import List
 
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        squares = [+i*i+j*j for (i, j) in points]
-        sorted_indexes = sorted((val, idx)
-                                for (idx, val) in enumerate(squares))
-        result = [points[idx] for (val, idx) in sorted_indexes[0:K]]
-        return result
+        sorted_points = points.sort(key=lambda p: p[0]*p[0]+p[1]*p[1])
+        return sorted_points[0:K]
 
 
 print(Solution().kClosest(points=[[3, 3], [5, -1], [-2, 4]], K=2))
