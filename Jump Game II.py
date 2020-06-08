76924506c6b446d3a0067926041412289a8ba956
dumavit <vitalii.duma@corva.ai>
2020-06-08 22:22:16 +0300

    more tasks

--- Jump Game II.py
+++ Jump Game II.py
@@ -1,14 +1,24 @@
+from typing import List
+
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
         size = len(nums)
-        visited = [False] * size
-        jumps = [[float('inf')]*size for i in range(size)]
-        queue = [0]
+        visited, jumps = [False] * size, [float('inf')]*size
+        queue, jumps[0] = [0], 0
+        print(len(nums))
+        op = 0
         while queue:
-            node = queue.pop()
-            print('node', node)
-            neighbours = 
+            node = queue.pop(0)
+            visited[node] = True
+            for neighbour in range(node+1, min(node+nums[node]+1, len(nums))):
+                op += 1
+                if not visited[neighbour]:
+                    queue.append(neighbour)
+                jumps[neighbour] = min(jumps[neighbour], jumps[node]+1)
+        print(op)
+        return jumps[-1]
 
 
-
-print(Solution().canJump([[2,3,1,1,4,0,0,1,2,1,5,0,0,1,2]]))
+print(Solution().jump([5, 8, 1, 1,1,1,1,1,1,1,1,2,3,1, 8, 9, 5, 3, 7, 2, 1, 8, 2, 3, 8, 9, 4, 7, 6, 2, 5, 2,
+                      7, 0, 6, 9, 9, 7, 3, 6, 3, 4, 8, 6, 4, 3, 3, 2, 7, 8, 5, 8, 6, 0]))
--- Queue Reconstruction by Height.py
+++ Queue Reconstruction by Height.py
@@ -1,19 +1,27 @@
 from typing import List
 
 
+# class Solution:
+#     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+#         sorted_people = sorted(people, key=lambda x: x[0])
+#         result = [None]*len(people)
+#         for (h, k) in sorted_people:
+#             idx, places = 0, k
+#             while places > 0 or result[idx] != None:
+#                 if result[idx] is None or result[idx][0] >= h:
+#                     places -= 1
+#                 idx += 1
+#             result[idx] = [h, k]
+#         return result
+
+
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        sorted_people = sorted(people, key=lambda x: x[0])
-        result = [None]*len(people)
-        for (h, k) in sorted_people:
-            idx, places = 0, k
-            while places > 0 or result[idx] != None:
-                if result[idx] is None or result[idx][0] >= h:
-                    places -= 1
-                idx += 1
-            result[idx] = [h, k]
-        return result
-
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
 
 print(Solution().reconstructQueue(
     [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]))
