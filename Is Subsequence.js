4a6a358992a91a217eb8279a64e0e12698242bc2
dumavit <vitalii.duma@corva.ai>
2020-06-09 10:41:47 +0300

    more tasks

---
+++ Is Subsequence.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    for (const char of t) {
+        if (s[0] === char) s = s.substr(1)
+        
+        if (!s) return true;
+    }
+    return !s;
+};
