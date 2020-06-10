65f65ce373401d430fd92a0457a4dc8e3f53b0cc
dumavit <vitalii.duma@corva.ai>
2020-06-10 10:18:04 +0300

    more tasks

--- Jump Game II.py
+++ Jump Game II.py
@@ -4,21 +4,14 @@ from typing import List
 class Solution:
     def jump(self, nums: List[int]) -> int:
         size = len(nums)
-        visited, jumps = [False] * size, [float('inf')]*size
-        queue, jumps[0] = [0], 0
-        print(len(nums))
-        op = 0
-        while queue:
-            node = queue.pop(0)
-            visited[node] = True
-            for neighbour in range(node+1, min(node+nums[node]+1, len(nums))):
-                op += 1
-                if not visited[neighbour]:
-                    queue.append(neighbour)
-                jumps[neighbour] = min(jumps[neighbour], jumps[node]+1)
-        print(op)
+        jumps = [float('inf')]*size
+        jumps[0] = 0
+        for i in range(size):
+            for j in range(1, nums[i]+1):
+                if i+j < size:
+                    jumps[i+j] = min(jumps[i+j], jumps[i]+1)
         return jumps[-1]
 
 
-print(Solution().jump([5, 8, 1, 1,1,1,1,1,1,1,1,2,3,1, 8, 9, 5, 3, 7, 2, 1, 8, 2, 3, 8, 9, 4, 7, 6, 2, 5, 2,
-                      7, 0, 6, 9, 9, 7, 3, 6, 3, 4, 8, 6, 4, 3, 3, 2, 7, 8, 5, 8, 6, 0]))
+print(Solution().jump([5, 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 1, 8, 9, 5, 3, 7, 2, 1, 8, 2, 3, 8, 9, 4, 7, 6, 2, 5, 2,
+                       7, 0, 6, 9, 9, 7, 3, 6, 3, 4, 8, 6, 4, 3, 3, 2, 7, 8, 5, 8, 6, 0]))
