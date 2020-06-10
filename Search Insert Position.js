167ef35cc607be35f0b4288ebd92dab263a55888
dumavit <vitalii.duma@corva.ai>
2020-06-10 10:19:50 +0300

    more tasks

---
+++ Search Insert Position.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let index = 0;
+    while (nums[index] < target) index += 1;
+    return index;
+};
