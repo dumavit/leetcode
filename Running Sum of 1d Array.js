ce05e2b87e3a66c6489cb80a65556319c65c7c7e
dumavit <vitalii.duma@corva.ai>
2020-10-14 12:00:23 +0300

    more tasks

---
+++ Running Sum of 1d Array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    const result = [nums[0]];
+    for (let i=1; i<nums.length; i++)
+        result.push(result[i-1]+ nums[i]);
+    return result;
+};
